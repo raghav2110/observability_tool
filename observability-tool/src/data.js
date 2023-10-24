ac69f3d3ab3bfc751e6c716d13c38edbbee9b6d0
Raghav Heda <raghav.heda@elastic.run>
2023-10-24 10:10:13 +0530

    [feat]: Added Line Graph Chart

---
+++ observability-tool/src/data.js
@@ -0,0 +1,13 @@
+const data = [
+    { endpoint: "Home", time: '2023-10-08T02:18:17.735Z', requests: 2364, special: true },
+    { endpoint: "Home", time: '2023-10-07T02:13:17.735Z', requests: 1132 },
+    { endpoint: "Home", time: '2023-10-06T02:03:17.735Z', requests: 3433, special: true },
+    { endpoint: "Product", time: '2023-10-08T02:18:17.735Z', requests: 3198, special: true },
+    { endpoint: "Product", time: '2023-10-07T02:13:17.735Z', requests: 1563 },
+    { endpoint: "Product", time: '2023-10-06T02:03:17.735Z', requests: 1563 },
+    { endpoint: "Contact", time: '2023-10-08T02:18:17.735Z', requests: 1950, special: true },
+    { endpoint: "Contact", time: '2023-10-07T02:13:17.735Z', requests: 2298, special: true },
+    { endpoint: "Contact", time: '2023-10-06T02:03:17.735Z', requests: 2800 },
+];
+
+export default data;
